3b5374a29edde6e80d1d8c47dc75c969a883803d
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-05-06 14:44:09 +0200

    go mod tidy, bump proto version and add new error codes

--- apierr/error.go
+++ apierr/error.go
@@ -2,6 +2,7 @@ package apierr
 
 import (
 	"errors"
+	"net/http"
 
 	"google.golang.org/grpc/codes"
 )
@@ -11,6 +12,9 @@ var ErrInvalidFile = errors.New("invalid file")
 var ErrNotImplemented = errors.New("not implemented")
 var ErrAlreadyExists = errors.New("already exists")
 var ErrUnexpected = errors.New("unexpected")
+var ErrBadRequest = errors.New("bad request")
+var ErrUnauthenticated = errors.New("unauthenticated")
+var ErrForbidden = errors.New("forbidden")
 var ErrInvalidRequest = errors.New("invalid request")
 var ErrValidationFailed = errors.New("validation failed")
 var ErrInvalidPassword = errors.New("invalid password")
@@ -19,40 +23,47 @@ var ErrFailedToGenerateCredentials = errors.New("failed to generate credentials"
 // HTTPStatusCode returns the HTTP status code for the given error
 func HTTPStatusCode(err error) int {
 	if errors.Is(err, ErrNotFound) {
-		return 404
+		return http.StatusNotFound
 	}
 
 	// Protobuf storage errors
 	// These are possible to remedy by the user so they are marked as 400
 	if errors.Is(err, ErrInvalidFile) ||
 		errors.Is(err, ErrValidationFailed) ||
-		errors.Is(err, ErrInvalidRequest) {
-		return 400
+		errors.Is(err, ErrInvalidRequest) ||
+		errors.Is(err, ErrBadRequest) {
+		return http.StatusBadRequest
 	}
 
 	// Authentication failures
 	if errors.Is(err, ErrInvalidPassword) ||
-		errors.Is(err, ErrFailedToGenerateCredentials) {
-		return 401
+		errors.Is(err, ErrFailedToGenerateCredentials) ||
+		errors.Is(err, ErrUnauthenticated) {
+		return http.StatusUnauthorized
+	}
+
+	// Authorization failure
+	if errors.Is(err, ErrForbidden) {
+		return http.StatusForbidden
 	}
 
 	// Not implemented error
 	if errors.Is(err, ErrNotImplemented) {
-		return 501
+		return http.StatusNotImplemented
 	}
 
 	// CONFLICT
 	if errors.Is(err, ErrAlreadyExists) {
-		return 409
+		return http.StatusConflict
 	}
 
 	// unexpected
 	if errors.Is(err, ErrUnexpected) {
-		return 500
+		return http.StatusInternalServerError
 	}
 
 	// still unexpected
-	return 500
+	return http.StatusInternalServerError
 }
 
 // GRPCCode returns the gRPC status code for an error.
@@ -66,14 +77,21 @@ func GRPCCode(err error) codes.Code {
 	// These are possible to remedy by the user so they are marked as 400
 	if errors.Is(err, ErrInvalidFile) ||
 		errors.Is(err, ErrValidationFailed) ||
+		errors.Is(err, ErrBadRequest) ||
 		errors.Is(err, ErrInvalidRequest) {
 		return codes.InvalidArgument
 	}
 
 	// Authentication failures
 	if errors.Is(err, ErrInvalidPassword) ||
+		errors.Is(err, ErrUnauthenticated) ||
 		errors.Is(err, ErrFailedToGenerateCredentials) {
 		return codes.Unauthenticated
+	}
+
+	// Authorization failure
+	if errors.Is(err, ErrForbidden) {
+		return codes.PermissionDenied
 	}
 
 	// Not implemented error
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/SecuritasCrimePrediction/apitools-go
 
-go 1.13
+go 1.16
 
 require (
 	github.com/Azure/azure-sdk-for-go v46.3.0+incompatible
@@ -14,18 +14,18 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/gogo/status v1.1.0
 	github.com/golang-migrate/migrate/v4 v4.11.0
+	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.1.2
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.4.0
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mennanov/fmutils v0.0.3
 	github.com/pkg/errors v0.9.1
-	go.uber.org/zap v1.15.0
-	golang.org/x/tools v0.0.0-20200914163123-ea50a3c84940 // indirect
+	go.uber.org/zap v1.16.0
 	google.golang.org/appengine v1.6.6
-	google.golang.org/genproto v0.0.0-20201022181438-0ff5f38871d5
-	google.golang.org/grpc v1.33.1
+	google.golang.org/genproto v0.0.0-20210426193834-eac7f76ac494
+	google.golang.org/grpc v1.37.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.0
-	google.golang.org/protobuf v1.25.0
+	google.golang.org/protobuf v1.26.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
